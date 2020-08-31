dda08c9c5636fba507674b3fe30427e1084222da
Tauriq Hendricks <tauriqh786@gmail.com>
2020-08-31 10:13:33 +0200

    Add project files.

---
+++ Acme.Biz/VendorRepository.cs
@@ -0,0 +1,155 @@
+﻿using Acme.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    public class VendorRepository
+    {
+        private List<Vendor> vendors;
+
+        /// <summary>
+        /// Retrieve one vendor.
+        /// </summary>
+        /// <param name="vendorId">Id of the vendor to retrieve.</param>
+        public Vendor Retrieve(int vendorId)
+        {
+            // Create the instance of the Vendor class
+            Vendor vendor = new Vendor();
+
+            // Code that retrieves the defined customer
+
+            // Temporary hard coded values to return 
+            if (vendorId == 1)
+            {
+                vendor.VendorId = 1;
+                vendor.CompanyName = "ABC Corp";
+                vendor.Email = "abc@abc.com";
+            }
+            return vendor;
+        }
+
+        public List<Vendor> Retrieve()
+        {
+            if (vendors == null)
+            {
+                vendors = new List<Vendor>
+                {
+                    new Vendor()
+                    {
+                        VendorId = 1,
+                        CompanyName = "ABC Corp",
+                        Email = "abc@gmail.com"
+                    },
+
+                    new Vendor()
+                    {
+                        VendorId = 12,
+                        CompanyName = "XYZ Inc",
+                        Email = "xyz@gmail.com"
+                    }
+                };
+            }
+
+            foreach (var vendor in vendors)
+            {
+                Console.WriteLine(vendor);
+                var states = new Dictionary<string, string>();
+                states.Add("CA", "California");
+                
+            }
+            return vendors;
+        }
+
+        /// <summary>
+        /// Retrieves all of the approved vendors
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<string, Vendor> RetrieveWithKeys()
+        {
+            var vendors = new Dictionary<string, Vendor>
+            {
+                {
+                    "ABC Corp", new Vendor
+                    {
+                        VendorId = 5,
+                        CompanyName = "ABC Corp",
+                         Email = "abc@gmail.com"
+                    }
+                },
+                {
+                    "XYX Inc", new Vendor()
+                    {
+                        VendorId = 12,
+                        CompanyName = "XYZ Inc",
+                        Email = "xyz@gmail.com"
+                    }
+                }
+            };
+
+
+            foreach (var element in vendors)
+            {
+                var key = element.Key;
+                var vendor = element.Value;
+                Console.WriteLine($"key: {key} value: {vendor}");
+            }
+
+            /*foreach (var vendor in vendors.Values)
+            {
+                Console.WriteLine(vendor);
+            }*/
+
+            /*foreach (var companyName in vendors.Keys)
+            {
+                Console.WriteLine(vendors[companyName]);
+            }*/
+
+            /*if (vendors.ContainsKey("ABCc"))
+            {
+                Console.WriteLine(vendors["ABCc"]);
+            }
+
+            if (vendors.TryGetValue("XYZ", out Vendor vendor))
+            {
+                Console.WriteLine(vendor);
+            }*/
+
+            return vendors;
+        }
+
+        public T RetrieveValue<T>(string sql, T defaultValue)
+        {
+            // Call the database to retrieve the value
+            // If no value is returned, return the default value
+            T value = defaultValue;
+
+            return value;
+        }
+
+
+
+
+
+
+
+
+
+        /// <summary>
+        /// Save data for one vendor.
+        /// </summary>
+        /// <param name="vendor">Instance of the vendor to save.</param>
+        /// <returns></returns>
+        public bool Save(Vendor vendor)
+        {
+            var success = true;
+
+            // Code that saves the vendor
+
+            return success;
+        }
+    }
+}
---
+++ Tests/Acme.BizTests/VendorRepositoryTests.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class VendorRepositoryTests
+    {
+        [TestMethod()]
+        public void RetrieveValueTest()
+        {
+            //Arrange
+            var repository = new VendorRepository();
+            var expected = 42;
+
+            //Act
+            var actual = repository.RetrieveValue<int>("Select ...", 42);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void RetrieveValueStringTest()
+        {
+            //Arrange
+            var repository = new VendorRepository();
+            var expected = "test";
+
+            //Act
+            var actual = repository.RetrieveValue<string>("Select ...", "test");
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void RetrieveValueObjectTest()
+        {
+            //Arrange
+            var repository = new VendorRepository();
+            var vendor = new Vendor();
+            var expected = vendor;
+
+            //Act
+            var actual = repository.RetrieveValue<Vendor>("Select ...", vendor);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void RetrieveTest()
+        {
+            // Arange
+            var repo = new VendorRepository();
+            var expected = new List<Vendor>
+                {
+                    new Vendor()
+                    {
+                        VendorId = 1,
+                        CompanyName = "ABC Corp",
+                        Email = "abc@gmail.com"
+                    },
+
+                    new Vendor()
+                    {
+                        VendorId = 12,
+                        CompanyName = "XYZ Inc",
+                        Email = "xyz@gmail.com"
+                    }
+                };
+
+            // Act
+            var actual = repo.Retrieve();
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void RetrieveWithKeysTest()
+        {
+            // Arrange
+            var repo = new VendorRepository();
+            var expected = new Dictionary<string, Vendor>
+            {
+                {
+                    "ABC Corp", new Vendor
+                    {
+                        VendorId = 5,
+                        CompanyName = "ABC Corp",
+                         Email = "abc@gmail.com"
+                    }
+                },
+                {
+                    "XYX Inc", new Vendor()
+                    {
+                        VendorId = 12,
+                        CompanyName = "XYZ Inc",
+                        Email = "xyz@gmail.com"
+                    }
+                }
+            };
+
+            // Act
+            var actual = repo.RetrieveWithKeys();
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
